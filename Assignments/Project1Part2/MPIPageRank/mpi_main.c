a50dce32b1cda17333ebb523ba5f42b886eb5a14
Matt Sacks <matt.s.sacks@gmail.com>
2011-02-01 19:00:04 -0500

    Reorganize repo and add in Part 2 materials

---
+++ Assignments/Project1Part2/MPIPageRank/mpi_main.c
@@ -0,0 +1,24 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/*   MPI PageRank							     */
+/*   B534 Project1						             */
+/*   Group:                                                                  */
+/*   Authors:   							     */
+/*   Emails:       						             */
+/*   Date:								     */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>             /* strtok() */
+#include <sys/types.h>          /* open() */
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>             /* getopt() */
+#include <mpi.h>
+
+#include "pagerank.h"
+
+int main(int argc, char **argv)
+{
+   
+}
