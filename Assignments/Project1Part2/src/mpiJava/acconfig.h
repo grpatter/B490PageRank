26e43b0add88fc5036c454e66bda20e380933c95
grpatter <grpatter@indiana.edu>
2011-02-04 00:09:13 -0500

    updating repo.
    Structure:
    repo->[assignments]->[assignment#] -> src, docs, resources (assigment specific files)
    
    src: is where we should do all of our work and place any dependencies, modules, additions, etc..
    resources: ALL resources for that specific assigment portion
    docs: self explanatory
    
    PLEASE try to avoid committing .class files (unless they are a provided resource)
    as well as your .classpath (since we will likely all have different working environments)
    
    If someone wants to changes this, please let's discuss it first, re-organizing things locally after every pull isn't much fun ;)
    
    Signed-off-by: grpatter <grpatter@indiana.edu>

--- Assignments/Project1Part2/src/mpiJava/acconfig.h
+++
@@ -1,3 +0,0 @@
-/* version number */
-
-#undef  VERSION
--- Assignments/Project1Part2/src/mpiJava/examples/PingPong/Clock.java
+++
@@ -1,8 +0,0 @@
-public class Clock {
-
-  // timer() returns seconds...
-
-  static double timer() {    
-    return (double) System.currentTimeMillis() / 1.0e3; 
-  }
-}
