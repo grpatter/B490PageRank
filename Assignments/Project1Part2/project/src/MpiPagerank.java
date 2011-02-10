668208f5d16682d2200eadc7be4b87d97005d0b0
Jonathan M. Stout <lostpunisher@gmail.com>
2011-02-10 15:21:25 -0500

    Updated MpiPagernak.java to newest version.

--- Assignments/Project1Part2/project/src/MpiPagerank.java
+++ Assignments/Project1Part2/project/src/MpiPagerank.java
@@ -1,109 +1,50 @@
 import java.io.*;
 import java.util.*;
 import mpi.*;
+import mpi.Intracomm.*;
+
 
 public class MpiPagerank {
-	public static void readPagerankInput(String file, int localUrlCount, int[][] adjacencyMatrix, Intracomm mpiComm) throws NumberFormatException, IOException {
-		int nodeId = mpiComm.Rank();
-		int worldNodeCount = mpiComm.Size();
-		int[] luc = new int[1];
+
+	public static int readPagerankInput(String file, int[][] AdjacencyMatrix) throws NumberFormatException, IOException {
 		
-		// Read in adjacencyMatrix
-		if (nodeId == 0) {
-			BufferedReader f = new BufferedReader(new FileReader(file));
-			int i = 0;
-			
-			while (f.ready()) {
-				String nodeInfo = f.readLine();
-				String[] s = nodeInfo.split(" ");
-				int[] adjacencyMatrixPart = new int[s.length];
-				
-				for (int j = 0; j < s.length; j++) {
-					adjacencyMatrix[i][j] = Integer.parseInt(s[j]);
-				}
-			}
-			luc[0] = adjacencyMatrix.length;
+	    BufferedReader f = new BufferedReader(new FileReader(file));
+	    int totalUrlCount = 0;
+	    int maxLineLength = 0;
+	    
+	    while (f.ready()) {
+		String[] adjacencyInfo = f.readLine().split(" ");
+		int infoLength = adjacencyInfo.length;
+		totalUrlCount++;
+
+		if (infoLength > maxLineLength) {
+		    maxLineLength = infoLength;
 		}
-		
-		// Broadcast total url count to all nodes.
-		mpiComm.Bcast(luc, 0, 1, MPI.INT, 0);
-		
-		localUrlCount = luc[0] / worldNodeCount;
-		int remUrlCount = luc[0] / worldNodeCount;
-		
-		if (nodeId == 0) {
-			int dest = 1;
-			int counter = 0;
-			
-			for (int i = 0; i < luc[0]; i++) {
-				counter++;
-				
-				if (counter >= 111) {
-					dest++;
-					counter = 0;
-				}
-				
-				if (dest < worldNodeCount - 1) {
-					//System.out.println("Sending... " + dest);
-					mpiComm.Send(adjacencyMatrix[i], 0, 100, MPI.INT, dest, 1);
-				} else {
-					mpiComm.Send(adjacencyMatrix[luc[0] - (localUrlCount + remUrlCount)], 0, 100, MPI.INT, dest, 1);
-					break;
-				}					
-			}
-		} else {
-			if (nodeId < worldNodeCount - 1) {
-				for (int i = 0; i < localUrlCount; i++) {
-					mpiComm.Recv(adjacencyMatrix[i], 0, 100, MPI.INT, 0, 1);
-				}
-				System.out.println("Worker node " + nodeId + ": recived data");
-			} else {
-				for (int i = 0; i < localUrlCount + remUrlCount; i++) {
-					mpiComm.Recv(adjacencyMatrix[i], 0, 100, MPI.INT, 0, 1);
-				}
-				System.out.println("one");
-			}
-			
+	    }
+
+	    for (int i = 0; i < totalUrlCount; i++) {
+		for (int j = 0; j < maxLineLength; j++) {
+		    AdjacencyMatrix[i][j] = -1;
 		}
-		
-		// TODO Assign part of the problem to localPagerank.
+	    }
+
+	    f.close();
+	    f = new BufferedReader(new FileReader(file));
+
+	    int amPointer = 0;
+	    while (f.ready()) {
+		String[] adjacencyInfo = f.readLine().split(" ");
+
+		for (int i = 0; i < adjacencyInfo.length; i++) {
+		    AdjacencyMatrix[amPointer][i] = Integer.parseInt(adjacencyInfo[i]);
+		}
+		amPointer++;
+	    }
+
+	    f.close();
+	    return totalUrlCount;
 	}
 
-	public static Double getPagerank(HashMap<Integer, ArrayList<Integer>> globalLinks, HashMap<Integer, Double> output) {
-		Iterator<Integer> link_iter = globalLinks.keySet().iterator();
-		ArrayList<Integer> tmpstack;
-		Double dangling = 0.0;
-
-		//Create and populate the temporary storage for Pageranks 
-		HashMap<Integer, Double> tmpPagerank  = new HashMap<Integer, Double>();
-		for(Integer i = 0; i < globalLinks.size(); i++){
-		    tmpPagerank.put(i, 0.0);
-		}
-
-		//Iterate through each web page in links
-		while(link_iter.hasNext()) {
-		    int linkKey = link_iter.next();
-		    tmpstack = globalLinks.get(linkKey);
-		    int outgoingLinkCount = tmpstack.size();
-		    Double tmp = 0.0;
-
-		    //Iterate through each of this webpage's neighbors and calculate their Pagerank based on this web pages current Pagerank and number of outgoing links 
-		    for(int j = 0; j < outgoingLinkCount; j++){
-			int neighbor = tmpstack.get(j);
-			tmp = tmpPagerank.get(neighbor) + output.get(linkKey)/(double)outgoingLinkCount;
-			tmpPagerank.put(neighbor, tmp);
-		    }
-
-		    //If this webpage has no outgoing links, calculate its contribution to the overall dangling value 
-		    if(outgoingLinkCount == 0){
-			dangling += output.get(linkKey);
-		    }
-		}
-		
-		// Return dangling value
-		return dangling;
-	}
-	
 	/**
 	 * @param args
 	 * @throws IOException 
@@ -113,53 +54,77 @@ public class MpiPagerank {
 		MPI.Init(args);
 		int nodeId = MPI.COMM_WORLD.Rank();
 		int globalNodeCount = MPI.COMM_WORLD.Size();
-		
 		String pagerankInputFilename = "pagerank.input";
-		
-		// Declare global variables.
+		int urlCount = 0;
+		Double PublicPR[] = new Double[1000];
+		Double PrivatePR[] = new Double[1000];
 		int[][] globalAdjacencyMatrix = new int[1000][100];
-		HashMap<Integer, Double> globalPagerank = new HashMap<Integer, Double>();
-		int globalUrlCount = 0;
-		// Declare local variables.
-		HashMap<Integer, Double> localPagerank = new HashMap<Integer, Double>();
-		int localUrlCount = 0;
-		Double localDanglingSum = 0.0;
-		
-		// Read in adjacency matrix from file
-		//readPagerankInput(pagerankInputFilename, globalAdjacencyMatrix, localPagerank);
-		//globalUrlCount = globalAdjacencyMatrix.size();
-		
+		Double dangling = 0.0;
 		Intracomm mpiComm = MPI.COMM_WORLD;
+		int iteration = 1;
+		// Read in adjacency matrix from file
+		urlCount = readPagerankInput(pagerankInputFilename, globalAdjacencyMatrix);
+		int localUrlCount = urlCount / globalNodeCount;
 		
-		readPagerankInput(pagerankInputFilename, localUrlCount, globalAdjacencyMatrix, mpiComm);
-		
-		// TODO Broadcast initial pagerank value to workers
-		
-		if (nodeId == 0) {
-			// Code for root.
-			System.out.println("Hello from king node " + nodeId + ". I rule over a kingdom of " + globalNodeCount + " nodes.");
+		//Populate the initial pagerank
+		for (int m = 0; m < globalAdjacencyMatrix.length; m++) {
+		    PublicPR[m] = 1.0/urlCount;  
+		    PrivatePR[m] = 1.0/urlCount;	
 
-			// TODO
-			
-			// TODO Receive slice of globalLinks back from workers and apply to globalLinks
-			
-			// TODO Receive dangle value from worker and add dangle/size to all nodes
-			
-			// TODO Apply damping factor to globalLinks
-			
-		} else {
-			// Code for worker.
-			System.out.println("I'm just a worker node named " + nodeId);
-			// TODO Receive data from root.
-
-			// TODO Find pagerank
-			//danglingValue = getPagerank(globalLinks, finalPagerank);
-			
-			// TODO Send slice of globalLinks to root
-			
-			// TODO Send dangle value
 		}
-		MPI.Finalize();
-	}
 
+
+		for (int iter = 0; iter < iteration; iter++) {
+		    int tracker = 1;			    
+		
+		    if (nodeId == 0) {
+			//System.out.println("Hello from king node " + nodeId + ". I rule over a kingdom of " + globalNodeCount + " nodes.");
+			
+			while (tracker <= localUrlCount) {
+			    for (int j = 1; j <= globalNodeCount; j++) {
+				int toBeSent[][] = new int[globalAdjacencyMatrix.length / globalNodeCount][globalAdjacencyMatrix[0].length];
+				for (int k = 0; k < globalAdjacencyMatrix[0].length; k++) {
+				    toBeSent[j - 1][k] = globalAdjacencyMatrix[(j * tracker) - 1][k]; 
+				}
+				System.out.println("This is the tracker: " + tracker);
+				Object ad = (Object)toBeSent;
+				Object send[] = new Object[1];
+				send[0] = ad;
+				System.out.println("Sending from: " + nodeId + " to: " + tracker );			
+				mpiComm.Send(send, 0, send.length, MPI.OBJECT, tracker, 1);			    
+			    }
+			    tracker++;
+			}
+			
+		    } else {
+			System.out.println("I'm just a worker node named " + nodeId);
+			
+			// TODO Receive data from root.
+			Object in[] = new Object[1];
+			System.out.println("Recieving...");
+			mpiComm.Recv(in, 0, 1, MPI.OBJECT, 0, MPI.ANY_TAG);
+			System.out.println("Recieved");
+			int[][] setToWork = new int[1][1];
+			setToWork = (int[][])in[0];//from object to 2d array
+			
+			//System.out.print(setToWork[0][1]);
+			for(int x = 0; x < setToWork.length; x++){
+			    int[] currentLinks = setToWork[x];
+			    int outgoingLinkCount = currentLinks.length;			
+			    //Iterate through each of this webpage's neighbors and calculate their Pagerank based on this web pages current Pagerank and number of outgoing links
+			    for(int j = 0; j < outgoingLinkCount; j++){
+				int neighbor = currentLinks[j];
+				if(neighbor != -1){
+				    PrivatePR[x] += PublicPR[neighbor]/outgoingLinkCount;				
+				} else { dangling += PrivatePR[x]; }
+			    }
+			}
+			for(int l = 0; l < urlCount; l++) {
+			    System.out.println(PrivatePR[l]);			
+			}
+			MPI.Finalize();
+		    }
+		    
+		}
+	}
 }
