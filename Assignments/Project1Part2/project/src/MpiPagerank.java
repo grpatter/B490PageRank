ee79e3ec4e23a76a1e352512c575cf0f443dfa4c
grpatter <grpatter@indiana.edu>
2011-02-11 00:18:48 -0500

    added an allreduce method that is sort of working... but giving back really fucked values (like the PRs are being populated correctly though I think they are..). It iterates over each node (total page nodes to calc) and performs and allreduce into its local buffer.

--- Assignments/Project1Part2/project/src/MpiPagerank.java
+++ Assignments/Project1Part2/project/src/MpiPagerank.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.security.AllPermission;
 import java.util.*;
 import mpi.*;
 import mpi.Intracomm.*;
@@ -54,10 +55,12 @@ public class MpiPagerank {
 		MPI.Init(args);
 		int nodeId = MPI.COMM_WORLD.Rank();
 		int globalNodeCount = MPI.COMM_WORLD.Size();
-		String pagerankInputFilename = "pagerank.input";
+		String pagerankInputFilename = "C:\\pagerank.input";
 		int urlCount = 0;
-		Double PublicPR[] = new Double[1000];
-		Double PrivatePR[] = new Double[1000];
+//		Double PublicPR[] = new Double[1000];
+//		Double PrivatePR[] = new Double[1000];
+		double PublicPR[] = new double[1000];
+		double PrivatePR[] = new double[1000];
 		int[][] globalAdjacencyMatrix = new int[1000][100];
 		Double dangling = 0.0;
 		Intracomm mpiComm = MPI.COMM_WORLD;
@@ -78,53 +81,93 @@ public class MpiPagerank {
 		    int tracker = 1;			    
 		
 		    if (nodeId == 0) {
-			//System.out.println("Hello from king node " + nodeId + ". I rule over a kingdom of " + globalNodeCount + " nodes.");
-			
-			while (tracker <= localUrlCount) {
-			    for (int j = 1; j <= globalNodeCount; j++) {
-				int toBeSent[][] = new int[globalAdjacencyMatrix.length / globalNodeCount][globalAdjacencyMatrix[0].length];
-				for (int k = 0; k < globalAdjacencyMatrix[0].length; k++) {
-				    toBeSent[j - 1][k] = globalAdjacencyMatrix[(j * tracker) - 1][k]; 
+				//System.out.println("Hello from king node " + nodeId + ". I rule over a kingdom of " + globalNodeCount + " nodes.");
+				
+				while (tracker <= localUrlCount) {
+				    for (int j = 1; j < globalNodeCount; j++) {
+						int toBeSent[][] = new int[globalAdjacencyMatrix.length / globalNodeCount][globalAdjacencyMatrix[0].length];
+						
+						for (int k = 0; k < globalAdjacencyMatrix[0].length; k++) {
+						    toBeSent[j - 1][k] = globalAdjacencyMatrix[(j * tracker) - 1][k]; 
+						}
+						
+						System.out.println("This is the tracker: " + tracker);
+						System.out.println("This is the j: " + j);
+						Object ad = (Object)toBeSent;
+						Object send[] = new Object[1];
+						send[0] = ad;
+						System.out.println("Sending from: " + nodeId + " to: " + tracker + " a set of size: " + toBeSent.length);			
+						mpiComm.Send(send, 0, send.length, MPI.OBJECT, j, 1);			    
+				    }
+				    tracker++;
+				    break;
 				}
-				System.out.println("This is the tracker: " + tracker);
-				Object ad = (Object)toBeSent;
-				Object send[] = new Object[1];
-				send[0] = ad;
-				System.out.println("Sending from: " + nodeId + " to: " + tracker );			
-				mpiComm.Send(send, 0, send.length, MPI.OBJECT, tracker, 1);			    
-			    }
-			    tracker++;
-			}
 			
 		    } else {
-			System.out.println("I'm just a worker node named " + nodeId);
-			
-			// TODO Receive data from root.
-			Object in[] = new Object[1];
-			System.out.println("Recieving...");
-			mpiComm.Recv(in, 0, 1, MPI.OBJECT, 0, MPI.ANY_TAG);
-			System.out.println("Recieved");
-			int[][] setToWork = new int[1][1];
-			setToWork = (int[][])in[0];//from object to 2d array
-			
-			//System.out.print(setToWork[0][1]);
-			for(int x = 0; x < setToWork.length; x++){
-			    int[] currentLinks = setToWork[x];
-			    int outgoingLinkCount = currentLinks.length;			
-			    //Iterate through each of this webpage's neighbors and calculate their Pagerank based on this web pages current Pagerank and number of outgoing links
-			    for(int j = 0; j < outgoingLinkCount; j++){
-				int neighbor = currentLinks[j];
-				if(neighbor != -1){
-				    PrivatePR[x] += PublicPR[neighbor]/outgoingLinkCount;				
-				} else { dangling += PrivatePR[x]; }
-			    }
-			}
-			for(int l = 0; l < urlCount; l++) {
-			    System.out.println(PrivatePR[l]);			
-			}
-			MPI.Finalize();
+				System.out.println("I'm just a worker node named " + nodeId);
+				
+				// TODO Receive data from root.
+				Object in[] = new Object[1];
+				System.out.println("Recieving...");
+				mpiComm.Recv(in, 0, 1, MPI.OBJECT, 0, MPI.ANY_TAG);
+				System.out.println("Recieved");
+				int[][] setToWork = new int[1][1];
+				setToWork = (int[][])in[0];//from object to 2d array
+				
+				//System.out.print(setToWork[0][1]);
+				for(int x = 0; x < setToWork.length; x++){
+				    int[] currentLinks = setToWork[x];
+				    int outgoingLinkCount = currentLinks.length;			
+				    //Iterate through each of this webpage's neighbors and calculate their Pagerank based on this web pages current Pagerank and number of outgoing links
+				    for(int j = 0; j < outgoingLinkCount; j++){
+					int neighbor = currentLinks[j];
+					if(neighbor != -1){
+					    PrivatePR[x] += PublicPR[neighbor]/outgoingLinkCount;	
+	//				    System.out.println("Setting PrPR["+x+"] to:" + PrivatePR[x]);
+					} else { dangling += PrivatePR[x]; }
+				    }
+				}
+				for(int l = 0; l < urlCount; l++) {
+	//			    System.out.println("PrPR["+l+"] is: " + PrivatePR[l]);			
+				}
+				System.out.println("dangling is: " + dangling);
 		    }
 		    
+
+			try {
+				MpiPagerank.allreduceTest(args, PrivatePR, PublicPR, urlCount);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			MPI.Finalize();
 		}
 	}
+	
+	  public static void allreduceTest(String[] args, double[] PrivatePR, double[]PublicPR, int urlCount) throws Exception {
+
+		    int j;
+		    int myself;
+		    int cur = 0,prev = 0;
+
+		    myself = MPI.COMM_WORLD.Rank();
+
+		    for(j=1;j<urlCount;j++)  {
+		      cur = j;
+//		      MPI.COMM_WORLD.Allreduce(out,0,in,0,j,MPI.INT,MPI.SUM);
+		      MPI.COMM_WORLD.Allreduce(PublicPR,0,PrivatePR,0,j,MPI.DOUBLE,MPI.SUM);
+		      
+		      MPI.COMM_WORLD.Barrier();
+		      if(cur != prev){
+		    	  System.out.println("Iter: " + j + " :PubPr[]/PrPr[] is: " + PublicPR[j] + " / " + PrivatePR[j]);
+		      }else{
+		    	  prev = cur;
+		    	  cur++;
+		      }
+		    }
+
+		    MPI.COMM_WORLD.Barrier();
+		    if(myself == 0)  System.out.println("Allreduce COMPLETE");
+		  }
 }
