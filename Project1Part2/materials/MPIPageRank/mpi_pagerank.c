d0903e8a68f25e1b1799a939b79593a16e3fc369
grpatter <grpatter@indiana.edu>
2011-02-18 17:03:35 -0500

    part2 of moving project dirs to root

---
+++ Project1Part2/materials/MPIPageRank/mpi_pagerank.c
@@ -0,0 +1,18 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/*   MPI PageRank							     */
+/*   B534 project1                                                           */
+/*   Group:        						             */
+/*   Authors: 							             */
+/*   Emails: 						                     */
+/*   Date:								     */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+
+#include "pagerank.h"
+
+int mpi_pagerank() {
+   
+}
