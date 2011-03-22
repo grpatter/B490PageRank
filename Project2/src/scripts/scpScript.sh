17dd9277b84792bacad061e589a00d6684c2efd2
grpatter <grpatter@indiana.edu>
2011-03-21 22:28:13 -0400

    more scripting so i dont die inside

--- Project2/src/scripts/scpScript.sh
+++ Project2/src/scripts/scpScript.sh
@@ -5,5 +5,12 @@ for i in 10.0.2.131 10.0.2.132
 do
   echo "SCP to Machine $i"
   scp -i ~/.ssh/id_rsa pr.zip root@$i:~/
+  echo "SSH to machine $i"
+  ssh $i
+  shopt -s extglob
+  rm !(pr.zip)
+  unzip pr.zip .
+  echo "Unzipped our stuffz"
+  logout  
 done
 echo "Done...hopefully!"
