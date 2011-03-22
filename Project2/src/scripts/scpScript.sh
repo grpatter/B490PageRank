d318d35b50aaa63a5616aeb5eaaddd535ff71f2c
grpatter <grpatter@indiana.edu>
2011-03-21 22:36:07 -0400

    not less pain, last commit was MOREEEEE pain. FRAK.

--- Project2/src/scripts/scpScript.sh
+++ Project2/src/scripts/scpScript.sh
@@ -3,16 +3,7 @@
 echo "Beginning SCP Ops"
 for i in 10.0.2.131 10.0.2.132
 do
-  echo "SSH to machine $i"
-  ssh $i
-  rm *
-  logout
   echo "SCP to Machine $i"
   scp -i ~/.ssh/id_rsa pr.zip root@$i:~/
-  echo "SSH to machine $i"
-  ssh $i
-  unzip pr.zip .
-  echo "Unzipped our stuffz"
-  logout  
 done
 echo "Done...hopefully!"
