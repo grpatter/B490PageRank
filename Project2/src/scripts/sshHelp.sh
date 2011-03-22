e3f8fbb30d2ae661aff3e5c5ebf5e862348741cb
grpatter <grpatter@indiana.edu>
2011-03-21 23:07:07 -0400

    ssh helper script

--- Project2/src/scripts/sshHelp.sh
+++ Project2/src/scripts/sshHelp.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a comment!
 echo "Beginning SSH Ops"
-	shopt -s extglob
-	rm !(pr.zip)
+	rm *
+	mv tmp/pr.zip .
 	unzip -u pr.zip
 	/opt/mpj-v0_38/bin/mpjdaemon_linux_x86_64 start
 	logout
