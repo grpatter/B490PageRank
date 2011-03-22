3f0756b0ad059356848e6810843af1e7dc69be44
grpatter <grpatter@indiana.edu>
2011-03-21 22:32:37 -0400

    maybe less pain..

--- Project2/src/scripts/scpScript.sh
+++ Project2/src/scripts/scpScript.sh
@@ -3,12 +3,14 @@
 echo "Beginning SCP Ops"
 for i in 10.0.2.131 10.0.2.132
 do
+  echo "SSH to machine $i"
+  ssh $i
+  rm *
+  logout
   echo "SCP to Machine $i"
   scp -i ~/.ssh/id_rsa pr.zip root@$i:~/
   echo "SSH to machine $i"
   ssh $i
-  shopt -s extglob
-  rm !(pr.zip)
   unzip pr.zip .
   echo "Unzipped our stuffz"
   logout  
