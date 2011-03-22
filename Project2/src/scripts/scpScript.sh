73e87394abf3601a14fdabb436df3c2cccaaaebc
grpatter <grpatter@indiana.edu>
2011-03-21 22:18:07 -0400

    script to save my life..

---
+++ Project2/src/scripts/scpScript.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# This is a comment!
+echo "Beginning SCP Ops"
+for i in 10.0.2.131 10.0.2.132
+do
+  echo "SCP to Machine $i"
+  scp -i ~/.ssh/id_rsa pr.zip root@$i:~/
+done
+echo "Done...hopefully!"
