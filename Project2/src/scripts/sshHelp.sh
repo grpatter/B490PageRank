cba76c3a58e7aee1d9739188ef145ad9b90fbdc7
grpatter <grpatter@indiana.edu>
2011-03-21 23:12:22 -0400

    stupid unzip...of course you should overwrite. QUIETLY. wtf.

--- Project2/src/scripts/sshHelp.sh
+++ Project2/src/scripts/sshHelp.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 # This is a comment!
 echo "Beginning SSH Ops"
-	mkdir tmp
-	mv pr.zip /tmp
-	rm *
-	mv tmp/pr.zip .
-	unzip -u pr.zip
+	unzip -o pr.zip
 	/opt/mpj-v0_38/bin/mpjdaemon_linux_x86_64 start
 	logout
