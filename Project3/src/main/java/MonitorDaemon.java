847abba97219f9b217a7a69256a7224bd925555b
Jonathan M. Stout <lostpunisher@gmail.com>
2011-04-06 17:38:34 -0400

    Updated MonitorDaemon. Added cmd line interface to kill monitorDaemon thread on command exit.

--- Project3/src/main/java/MonitorDaemon.java
+++ Project3/src/main/java/MonitorDaemon.java
@@ -1,9 +1,18 @@
 package main.java;
 
+import java.util.Scanner;
+
 public class MonitorDaemon {
+	private MonitorWorker mWorker;
 	
 	public MonitorDaemon(String host, String port, String clusterName, int daemonNo) {
-		
+		this.mWorker = new MonitorWorker(host, port, clusterName, daemonNo);
+		Thread daemonThread = new Thread(mWorker);
+		daemonThread.start();
+	}
+	
+	public void terminateWorker() {
+		mWorker.terminate();
 	}
 	
 	/**
@@ -11,8 +20,20 @@ public class MonitorDaemon {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		boolean running = true;
+		Scanner in = new Scanner(System.in);
+		
 		MonitorDaemon daemon = new MonitorDaemon(brokerHost, brokerPort, clusterName, daemonNo);
-		daemon.run();
-	}
 
+		while(running) {
+			String input = in.nextLine();
+			
+			if (input.compareTo("exit") == 0) {
+				daemon.terminateWorker();
+				running = false;
+			} else {
+				System.out.println(input);
+			}
+		}
+	}
 }
