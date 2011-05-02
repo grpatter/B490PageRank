c0a09d2291c0d384d85e7ee4c03d51f00189845b
Jonathan M. Stout <lostpunisher@gmail.com>
2011-05-01 21:26:44 -0400

    Script now launches monitor Daemons.

--- Project4/Scripts/copy.sh
+++ Project4/Scripts/copy.sh
@@ -6,7 +6,7 @@
 echo "unzipping archive"
 unzip -o source34.zip
 
-echo "running java"
-/N/soft/jdk1.6.0_20-x86-64/jre/bin/java -verbose -Djava.library.path=~/Project4/Daemon/Daemon_linux_lib -jar ~/Project4/Daemon/Daemon.jar
+echo "Starting monitor Daemons"
+/N/soft/jdk1.6.0_20-x86_64/jre/bin/java -verbose -Djava.library.path=~/Project4/Daemon/Daemon_linux_lib -jar ~/Project4/Daemon/Daemon.jar
 
-#java -Xms64m -Xmx512m -jar Daemon.jar
+echo "Starting mpi-pagerank"
--- Project4/Scripts/vmScript.sh
+++ Project4/Scripts/vmScript.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 #
-# p4-test.sh
+# vmScript.sh
 #
 # Set up grid environment
 #PBS -q b534
-#PBS -l nodes=2:ppn=8,walltime=00:13:00
+#PBS -l nodes=i87+i88:ppn=8,walltime=00:13:00
 #PBS -o run_pagerank_bm$PBS_JOBID.out
+#PBS -V
 
 #print the time and date
 date
@@ -15,20 +16,17 @@ echo "Starting VM's"
 
 echo "Waiting for VM's"
 /usr/local/bin/wait_for_vms $VM_NODEFILE
+cat $VM_NODEFILE > machines.txt
 
 echo "Distributing source"
-
 for line in `cat $VM_NODEFILE`
 do
-
-  echo "copy to: $line"
+  echo "Copy files to: $line"
   scp -v -i ~/.ssh/id_rsa source34.zip jonstout@$line:~/
 
   echo "ssh for copy.sh"
   ssh -v -i ~/.ssh/id_rsa jonstout@$line 'bash -s' < copy.sh
 done
-
-sleep 10
 
 # OUR CODE HERE
 echo "Shutting down VM's"
