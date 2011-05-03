2e9c9f0ebf9a7eca852fa3abc61e01676a11e1b9
Jonathan M. Stout <lostpunisher@gmail.com>
2011-05-02 22:24:35 -0400

    ...

--- Project4/Scripts/copy.sh
+++ Project4/Scripts/copy.sh
@@ -7,10 +7,15 @@ unzip -o Source.zip
 echo "Starting monitor Daemons"
 #/N/soft/jdk1.6.0_20-x86_64/jre/bin/java -verbose -Djava.library.path=~/Project4/Daemon/Daemon_linux_lib -jar ~/Project4/Daemon/Daemon.jar
 
-echo "Compiling mpi-pagerank"
-javac -cp .:$MPJ_HOME/lib/mpj.jar Pagerank/java/PagerankMpi_updated.java Pagerank/java/PerformanceLogger.java -Xlint:unchecked
 
-#/N/soft/jdk1.6.0_20-x86_64/bin/javac -cp .:$MPJ_HOME/lib/mpj.jar Pagerank/java/PagerankMpi_updated.java Pagerank/java/PerformanceLogger.java -Xlint:unchecked
+echo "Compiling mpi-pagerank"
+#javac -cp .:$MPJ_HOME/lib/mpj.jar Pagerank/java/PagerankMpi_updated.java Pagerank/java/PerformanceLogger.java -Xlint:unchecked
+
+/N/soft/jdk1.6.0_20-x86_64/bin/javac -cp .:$MPJ_HOME/lib/mpj.jar Pagerank/java/PagerankMpi_updated.java Pagerank/java/PerformanceLogger.java -Xlint:unchecked
+
 
 echo "Running mpi-pagerank"
 mpjrun.sh -Xms512M -Xmx2048m --mca btl_tcp_if_exclude lo,eth1 -np 2 --hostfile machines.txt Pagerank/java/PagerankMpi_updated Pagerank/resources/urls100k0 testout 10 .85
+
+# I don't think this is the correct version.
+#mpjrun.sh -Xms512M -Xmx2048m --mca btl_tcp_if_exclude lo,eth1 -np 2 Pagerank/java/PagerankMpi_updated Pagerank/resources/urls100k0 testout 10 .85
